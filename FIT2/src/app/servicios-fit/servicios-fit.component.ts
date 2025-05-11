0b5e075456c6a237874232fc44ee471358ee1203
ChrisKeo0MG <al349312@edu.uaa.mx>
2025-05-11 15:26:21 -0600

    cambios diseño y validaciones

--- FIT2/src/app/servicios-fit/servicios-fit.component.ts
+++ FIT2/src/app/servicios-fit/servicios-fit.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
+
 
 interface Cliente {
   nombre: string;
@@ -13,7 +16,7 @@ interface Cliente {
 @Component({
   selector: 'app-servicios-fit',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './servicios-fit.component.html',
   styleUrl: './servicios-fit.component.css'
 })
@@ -32,21 +35,44 @@ export class ServiciosFITComponent {
     this.fechaActual = hoy.toISOString().split('T')[0];
   }
 
+  mostrarMensaje(titulo: string, texto: string, icono: 'success' | 'error'): void {
+    Swal.fire({
+      title: titulo,
+      text: texto,
+      icon: icono,
+      confirmButtonText: 'Aceptar'
+    });
+  }
+
   agregarCliente(): void {
-    if (!this.nombre || !this.edad || !this.membresia || !this.genero || !this.fechaRegistro) {
-      alert('Por favor completa todos los campos.');
+    if (!this.nombre) {
+      this.mostrarMensaje('Error', '¡Por favor coloca tu nombre.!', 'error');
       return;
     }
     if (this.edad < 10 || this.edad > 100) {
-      alert('La edad debe estar entre 10 y 100 años.');
+      this.mostrarMensaje('Error', '¡¡La edad debe estar entre 10 y 100 años.!!', 'error');
       return;
     }
 
+    if (!this.membresia) {
+      this.mostrarMensaje('Error', 'Por favor coloca la membresia que deseas.', 'error');
+      return;
+    }
+
+    if (!this.genero) {
+      this.mostrarMensaje('Error', '!Por favor selecciona tu genero!', 'error');
+      return;
+    }
+
+    if (!this.fechaRegistro) {
+      this.mostrarMensaje('Error', '¡¡Por favor colooca la fecha de tu registro.!!', 'error');
+      return;
+    }
     const [year, month, day] = this.fechaRegistro.split('-').map(Number);
-    const fecha = new Date(year, month - 1, day); 
+    const fecha = new Date(year, month - 1, day);
     const diaSemana = fecha.getDay();
     if (diaSemana === 0) {
-      alert('No se puede registrar un cliente en domingo.');
+      this.mostrarMensaje('Error', 'No se puede registrar un cliente en domingo.', 'error');
       return;
     }
 
@@ -62,7 +88,7 @@ export class ServiciosFITComponent {
     this.clientes.push(nuevoCliente);
     localStorage.setItem('clientes', JSON.stringify(this.clientes));
     this.borrarFormulario();
-    alert('Cliente agregado correctamente.');
+    this.mostrarMensaje('Éxito', 'Cliente agregado correctamente.', 'success');
   }
 
   borrarFormulario(): void {
